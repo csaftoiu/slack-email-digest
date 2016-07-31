3f8ddd9a761fc080fa0a1718bd2886089d6b4af9
Claudiu <csaftoiu@gmail.com>
2016-07-31 15:37:20 -0700

    requirements & package script

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+
+setup(
+    name='slack-email-digest',
+    version='0.1',
+    author='Claudiu Saftoiu',
+    author_email='csaftoiu@gmail.com',
+    description='Export Slack history as a rich HTML email message',
+    url='https://github.com/csaftoiu/slack-email-digest',
+
+    install_requires=[
+        'slacker(==0.9.24)',
+        'docopt(==0.6.2)',
+        'schema(==0.6.2)',
+    ],
+
+    packages=[],
+    scripts=['scripts/slack-email-digest.py'],
+)
