aac480cb5fa6b3670a565ae1bd72e4ed028543e1
Sridhar Ratnakumar <srid@heroku.com>
2016-08-06 20:43:17 -0400

    Do not print token

--- scripts/slack-email-digest.sh
+++ scripts/slack-email-digest.sh
@@ -1,3 +1,9 @@
-#!/bin/sh -xe
+#!/bin/sh -e
 
-python scripts/slack-email-digest.py --from="${SLACKEMAILDIGEST_FROM}" --to="${SLACKEMAILDIGEST_TO}" --token "${SLACKEMAILDIGEST_TOKEN}" --delivery=${SLACKEMAILDIGEST_DELIVERY}
+echo "Running slack-email-digest.py with from=${SLACKEMAILDIGEST_FROM} to=${SLACKEMAILDIGEST_TO} delivery=${SLACKEMAILDIGEST_DELIVERY}"
+
+python scripts/slack-email-digest.py \
+    --from="${SLACKEMAILDIGEST_FROM}" \
+    --to="${SLACKEMAILDIGEST_TO}" \
+    --token "${SLACKEMAILDIGEST_TOKEN}" \
+    --delivery=${SLACKEMAILDIGEST_DELIVERY}
