6afe9aad13f5462b812f73c379cf3ca86a1461a3
Sridhar Ratnakumar <srid@heroku.com>
2016-08-06 20:39:34 -0400

    Quoting

--- scripts/slack-email-digest.sh
+++ scripts/slack-email-digest.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -xe
 
-python scripts/slack-email-digest.py --from=${SLACKEMAILDIGEST_FROM} --to=${SLACKEMAILDIGEST_TO} --token ${SLACKEMAILDIGEST_TOKEN} --delivery=${SLACKEMAILDIGEST_DELIVERY}
+python scripts/slack-email-digest.py --from="${SLACKEMAILDIGEST_FROM}" --to="${SLACKEMAILDIGEST_TO}" --token "${SLACKEMAILDIGEST_TOKEN}" --delivery=${SLACKEMAILDIGEST_DELIVERY}
